aed0ee38e2e9679879f3075366859497c93d39d8
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-04-22 14:49:02 +0530

    add slick slider in section 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,40 @@ const menulist = document.querySelector(".menu-list");
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("showlist");
 });
+
+$('.slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1,
+    speed: 3000,
+    dots: false,
+    arrows: false,
+    cssEase: 'linear',
+    waitForAnimate: true,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
